fd8b9f538dd66f635db2d11dc424eca1393bdfed
zimmerman.k@icloud.com <>
2018-12-20 12:06:54 -0500

    12.20.18

---
+++ Emailapp/src/emailapp/Emailapp.java
@@ -0,0 +1,11 @@
+package emailapp;
+
+public class Emailapp {
+
+	public static void main(String[] args) {
+		Email em1 = new Email("John","Smith");
+		System.out.println(em1.showInfo());
+
+	}
+
+}
